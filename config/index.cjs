b531602fb3cf6a378d584f3630293ced996f7e36
Ernesting1203 <119719277+Ernesting1203@users.noreply.github.com>
2022-12-03 14:19:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小尹大宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o-5Fes_csOx9DWBAO3_NGzF-jQOs',
+      id: 'o-5Fes2oweAlVXMA7G4WLscmITv0',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '7A3X-tLO6JzK7cGSHj2Z96XSNa5fCxaxiPZ9czECODE',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
